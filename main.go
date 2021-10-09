e090213ede50a47d714dc06fc563c01392dd885b
Atharva-Gundawar <atharva.n.gundawar@gmail.com>
2021-10-09 10:41:51 +0530

    [FEAT] Inital paths

---
+++ main.go
@@ -0,0 +1,15 @@
+import “github.com/gofiber/fiber”
+
+func main() {
+	app := fiber.New()
+	// User paths
+	app.Post("/user", createuser)  				// Create new user
+	app.Get("/user/:id?", getuser) 				// Get exsisting user by user id
+	
+	// Post paths
+	app.Post("/posts", createuser)  			// Create new post
+	app.Get("/posts/:id?", getuser)				// Get existing post by post id
+	app.Get("/posts/users/:id?", getuser)		// Get all the posts of a user using user id
+	
+	app.Listen(port)
+  }
